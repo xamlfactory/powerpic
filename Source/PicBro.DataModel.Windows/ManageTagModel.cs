f8f1941e4854ba44186dea026d77a9a3cd92448d
Karthikeyan <pkarthi365@gmail.com>
2014-05-01 17:43:29 +0530

    ManageTagModel-Added

---
+++ Source/PicBro.DataModel.Windows/ManageTagModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace PicBro.DataModel.Windows
+{
+    public class ManageTagsModel : INotifyPropertyChanged
+    {
+
+        private string tag;
+
+        public string Tag
+        {
+            get
+            {
+                return tag;
+            }
+            set
+            {
+                tag = value;
+                this.RaisePropertyChanged("Tag");
+            }
+        }
+
+
+        private int images;
+
+        public int Images
+        {
+            get
+            {
+                return images;
+            }
+            set
+            {
+                images = value;
+                this.RaisePropertyChanged("Images");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
