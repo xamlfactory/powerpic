2f4c1d5e0c758a6711a21ce639eac9fd58d01ca2
Karthikeyan <pkarthi365@gmail.com>
2014-05-01 10:40:58 +0530

    AddTags Window file added

---
+++ Source/PicBro.Shell.Windows/Views/AddTagsWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using MahApps.Metro.Controls;
+using Microsoft.Practices.Prism.Events;
+using PicBro.DAL.Windows;
+using PicBro.DataModel.Windows;
+using PicBro.Shell.Windows.ViewModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PicBro.Shell.Windows.Views
+{
+    /// <summary>
+    /// Interaction logic for AddTagsWindow.xaml
+    /// </summary>
+    public partial class AddTagsWindow : MetroWindow
+    {
+        IEnumerable selectedImages;
+        IDataServiceProxy dataService;
+        public AddTagsWindow()
+        {
+            InitializeComponent();
+        }
+       
+
+        public AddTagsWindow(IEventAggregator eventagregator,IDataServiceProxy dataservice,object selectedImages)
+        {
+            InitializeComponent();
+            this.selectedImages = selectedImages as IEnumerable;
+            this.dataService = dataservice;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.selectedImages != null && this.dataService != null)
+            {
+                string[] tags = this.tagsTextBox.Text.Split(',');
+                for (int i = 0; i < tags.Length; i++)
+                {
+                    foreach (ImageModel selectediamge in selectedImages)
+                    {
+                        if (selectediamge != null)
+                        {
+                            this.dataService.InsertTag(tags[i], selectediamge.ID);
+                        }
+                    }
+                }
+            }
+            this.Close();
+        }
+    }
+}
