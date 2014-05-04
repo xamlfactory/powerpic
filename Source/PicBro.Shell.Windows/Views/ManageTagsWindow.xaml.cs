b2468cc8670a55b64151b966c1bb0b1a37de757e
Karthikeyan <pkarthi365@gmail.com>
2014-05-04 20:23:43 +0530

    DataGrid Replaced the GridView

--- Source/PicBro.Shell.Windows/Views/ManageTagsWindow.xaml.cs
+++ Source/PicBro.Shell.Windows/Views/ManageTagsWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MahApps.Metro.Controls;
+using Microsoft.Practices.Prism.Commands;
 using PicBro.DAL.Windows;
 using PicBro.DataModel.Windows;
 using System;
@@ -24,6 +25,9 @@ namespace PicBro.Shell.Windows.Views
     public partial class ManageTagsWindow : MetroWindow
     {
 
+        #region Properties
+
+        #region Tags
 
         public ObservableCollection<ManageTagsModel> Tags
         {
@@ -35,6 +39,61 @@ namespace PicBro.Shell.Windows.Views
         public static readonly DependencyProperty TagsProperty =
             DependencyProperty.Register("Tags", typeof(ObservableCollection<ManageTagsModel>), typeof(ManageTagsWindow), new PropertyMetadata(null));
 
+        #endregion
+
+        #region SearchCommand
+
+
+
+        public DelegateCommand SearchCommand
+        {
+            get { return (DelegateCommand)GetValue(SearchCommandProperty); }
+            set { SetValue(SearchCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SearchCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SearchCommandProperty =
+            DependencyProperty.Register("SearchCommand", typeof(DelegateCommand), typeof(ManageTagsWindow), new PropertyMetadata(null));
+        
+        #endregion
+
+        #region SearchText
+
+        public string SearchText
+        {
+            get { return (string)GetValue(SearchTextProperty); }
+            set { SetValue(SearchTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SearchText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SearchTextProperty =
+            DependencyProperty.Register("SearchText", typeof(string), typeof(ManageTagsWindow), new PropertyMetadata(string.Empty));
+        
+        #endregion
+
+        #region ClearSearchCommand
+
+
+
+
+        public DelegateCommand ClearSearchCommand
+        {
+            get { return (DelegateCommand)GetValue(ClearSearchCommandProperty); }
+            set { SetValue(ClearSearchCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ClearSearchCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ClearSearchCommandProperty =
+            DependencyProperty.Register("ClearSearchCommand", typeof(DelegateCommand), typeof(ManageTagsWindow), new PropertyMetadata(null));
+
+        
+
+        #endregion
+
+
+
+        #endregion
+
 
 
         private IDataServiceProxy dataService;
@@ -45,15 +104,35 @@ namespace PicBro.Shell.Windows.Views
         public ManageTagsWindow(IDataServiceProxy dataservice)
         {
             InitializeComponent();
+            this.IntializeCommands();
             this.DataContext = this;
             this.dataService = dataservice;
+            GetAllTags();
+
+        }
+
+        private void GetAllTags()
+        {
             var tags = this.dataService.GetTags();
-            this.Tags =tags.Result;
+            this.Tags = new ObservableCollection<ManageTagsModel>(tags.Result.OrderByDescending(tag => tag.Images));
+        }
+
+        private void IntializeCommands()
+        {
+            this.SearchCommand = new DelegateCommand(this.OnSearchCommand);
+            this.ClearSearchCommand = new DelegateCommand(this.GetAllTags);
+        }
+
+        private void OnSearchCommand()
+        {
+            var searchedTags = this.dataService.SearchTag(this.SearchText);
+            this.Tags = new ObservableCollection<ManageTagsModel>(searchedTags.Result.OrderByDescending(tag => tag.Images));
+
         }
 
         private void tagList_KeyUp(object sender, KeyEventArgs e)
         {
-            var selectedTags = new ObservableCollection<string>(this.tagList.SelectedItems.OfType<ManageTagsModel>().Select(selecteditem => selecteditem.Tag)); 
+            var selectedTags = new ObservableCollection<string>(this.tags_grid.SelectedItems.OfType<ManageTagsModel>().Select(selecteditem => selecteditem.Tag)); 
             if (e.Key == Key.Delete)
             {
                 var dialogResult = MessageBox.Show("Do you want to Remove Tag", "Remove Tag", MessageBoxButton.OKCancel);
@@ -69,8 +148,6 @@ namespace PicBro.Shell.Windows.Views
                     }
                 }               
             }
-        }
-
-      
+        }      
     }
 }
